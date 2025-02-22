32144b46e2ef76bddd8906eabee289a8bfd3c773
‮ <gxousos@gmail.com>
2025-02-22 15:47:40 +0200

    fix: missleading optimization comment

--- examples/working_model.cpp
+++ examples/working_model.cpp
@@ -4,7 +4,7 @@ Esse arquivo contém só o código para carregar um modelo de forma fixa no devi
 
 #define NumberOf(arg) ((unsigned int)(sizeof(arg) / sizeof(arg[0]))) // calculates the number of layers (in this case 4)
 
-#define _1_OPTIMIZE 0B00000001 // Highly-Recommended Optimization For RAM
+#define _1_OPTIMIZE 0B00000001 // USE_64_BIT_DOUBLE
 #define _2_OPTIMIZE 0B00100000 // MULTIPLE_BIASES_PER_LAYER 
 
 
